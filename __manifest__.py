95eee4c2c2e936339b530759ca521917a5ad7c4e
Hirae <hiraetari@protonmail.com>
2020-05-10 17:33:59 +0300

    Started making crypto app

---
+++ __manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Crypto App',
+    'description': 'Sign files and validate signed files',
+    'author': 'Kumiko Hiraetari',
+    'depends': ['base'],
+    'application': 'true',
+    'data': [
+        'views/crypto_menu.xml',
+    ],
+}
