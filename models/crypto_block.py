db09031d1c2aeb2a2dbd0feb7ec0ae520f724889
Hirae <hiraetari@protonmail.com>
2020-05-10 19:34:46 +0300

    added models

---
+++ models/crypto_block.py
@@ -0,0 +1,8 @@
+from odoo import fields, models
+
+class Block(models.Model):
+    _name = 'crypto.block'
+    _description = 'Block'
+    keypair_path= fields.Char()
+    active = fields.Boolean('Active?', default=True)
+    date_generated = fields.Date()
