7b342ac6db63154c96806c098aeb21d35680d605
Hirae <hiraetari@protonmail.com>
2020-05-22 16:26:08 +0300

    added search and groups

--- models/crypto_block.py
+++ models/crypto_block.py
@@ -3,6 +3,7 @@ from odoo import fields, models
 class Block(models.Model):
     _name = 'crypto.block'
     _description = 'Block'
-    keypair_path= fields.Char()
+    keypair_name = fields.Char()
+    keypair_path = fields.Char()
     active = fields.Boolean('Active?', default=True)
     date_generated = fields.Date()
