4c12929105ec980a499573b96da34665714ef6e5
Hirae <hiraetari@protonmail.com>
2020-05-26 14:15:57 +0300

    removed unneeded views

--- models/crypto_block.py
+++ models/crypto_block.py
@@ -3,7 +3,8 @@ from odoo import fields, models
 class Block(models.Model):
     _name = 'crypto.block'
     _description = 'Block'
-    keypair_name = fields.Char()
+
+    keypair_name = fields.Char('Title', required=True)
     keypair_path = fields.Char()
     active = fields.Boolean('Active?', default=True)
     date_generated = fields.Date()
