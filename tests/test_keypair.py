f09e02bc57833a014bdaf4689e096542ad284988
greendilow <54859905+greendilow@users.noreply.github.com>
2020-05-26 17:17:08 +0300

    Update test_keypair.py

--- tests/test_keypair.py
+++ tests/test_keypair.py
@@ -3,6 +3,8 @@ from odoo.tests.common import TransactionCase
 class TestBlock(TransactionCase):
     def setUp(self, *args, **kwargs):
         result = super().setUp(*args, **kwargs)
+        user_admin = self.env.ref('base.user_admin')
+        self.env= self.env(user=user_admin)
         self.Block = self.env['crypto.block']
         self.block_ode = self.Block.create({
         'keypair_name': 'Test crypto block',
@@ -11,3 +13,6 @@ class TestBlock(TransactionCase):
     def test_create(self):
         "Test Ketpairs are active by default"
         self.assertEqual(self.block_ode.active, True)
+    def test_check_keypair_name(self):
+        "Check valid Keypair Name"
+        self.assertTrue(self.block_ode._check_keypair_name)
