8fe2a28fce5fe7e9ef303a6791c492f0afbc1e76
Hirae <hiraetari@protonmail.com>
2020-05-26 15:13:36 +0300

    tried to fix model

--- models/crypto_block.py
+++ models/crypto_block.py
@@ -4,7 +4,7 @@ class Block(models.Model):
     _name = 'crypto.block'
     _description = 'Block'
 
-    keypair_name = fields.Char()
+    name = fields.Char('Name', required=True)
     keypair_path = fields.Char()
     active = fields.Boolean('Active?', default=True)
     date_generated = fields.Date()
